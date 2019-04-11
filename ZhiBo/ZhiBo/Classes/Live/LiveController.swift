811d61104c2bf2945339c579e9f3d2c808a786c7
hfx439 <1562067171@qq.com>
2019-04-11 17:50:09 +0800

    目录划分

---
+++ ZhiBo/ZhiBo/Classes/Live/LiveController.swift
@@ -0,0 +1,19 @@
+//
+//  LiveController.swift
+//  ZhiBo
+//
+//  Created by HFX on 2019/4/11.
+//  Copyright © 2019 HFX. All rights reserved.
+//
+
+import UIKit
+
+class LiveController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.white
+    }
+
+}
---
+++ ZhiBo/ZhiBo/Classes/Tools/NavigationController.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationController.swift
+//  ZhiBo
+//
+//  Created by HFX on 2019/4/11.
+//  Copyright © 2019 HFX. All rights reserved.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
